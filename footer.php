d97f2708d7ffe2bc434cefefe6738b342fab6de3
Spandan <dhungelspandan@gmail.com>
2017-01-23 13:49:55 +0545

    All files Added

---
+++ footer.php
@@ -0,0 +1,15 @@
+  <footer class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 footer">
+                    <p class="copyright">Copyright © 2016 Company Name</p>
+                </div>  
+                <?php wp_footer(); ?>  
+            </footer>  <!-- .row -->      
+        </div> <!-- .container -->
+    </div> <!-- .main-body -->
+
+    <!-- JavaScript -->
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.11.3.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+
+</body>
+</html>
