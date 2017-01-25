e2a8a6096fe0ba36f4e2d2f9e4616dfd13b2a796
Spandan <dhungelspandan@gmail.com>
2017-01-25 14:04:05 +0545

    header modified

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accord - Free Bootstrap Template</title>
     <?php wp_head(); ?>
 </head>
 <body>
@@ -85,4 +84,4 @@
                                 </div> <!-- .mauris -->
                             </div>
                         </div> <!-- main-menu -->
-                    </aside> <!-- main-navigation -->
+                    </aside> <!-- main-navigation -->`
